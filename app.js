427d5f76f419684618539b42bb3469efe2bcfcc4
Daniel Gooden <dan.gooden.dev@gmail.com>
2023-04-27 15:18:36 +1000

    setting up the logic in JS to create a board and players for the game

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-const test = document.createElement("h1");
-const body = document.querySelector("body");
+const GameBoard = function () {
+  // prettier-ignore
+  this.board = [
+    null, null, null, 
+    null, null, null, 
+    null, null, null
+  ];
 
-test.textContent = "Hello World";
-body.appendChild(test);
+  this.playMove = (i, side) => {
+    this.board[i] = side;
+    console.log(this.board);
+  };
+};
+
+const playerFactory = (side) => {
+  let score = 0;
+
+  const getScore = () => score;
+  const getSide = () => side;
+  const win = () => {
+    score += 1;
+    return `${side} wins`;
+  };
+  const newGame = () => {
+    score = 0;
+    return "Reset score to 0";
+  };
+
+  return { getSide, win, getScore, newGame };
+};
+
+const x = playerFactory("x");
+const o = playerFactory("o");
+
+const gameBoard = new GameBoard();
+
+
